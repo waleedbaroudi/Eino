6083a87a6f82c120e42ca62d37873cdc0bbc4031
poufy <nooraldeenalfayad@hotmail.com>
2020-04-19 08:15:45 +0300

    initial http server with root end-point

---
+++ back-end/server/app.js
@@ -0,0 +1,70 @@
+import http from 'http';
+import express from 'express';
+import morgan from 'morgan';
+
+//Server Setup
+const port = 3000 || process.env.PORT;
+const app = express();
+const server = http.createServer(app);
+
+//Express middlewear
+//Funnel all requests through morgan to be logged to the console.
+app.use(morgan("dev"));
+
+//Parse the data from the requests we get so we can extract them.
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//To prevent CORS errors
+//We need to append headers before the response is sent back to the client
+//These headers tell the browser that we allow a client has a different origin from our server to get the response.
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    //* Every origin that a request comes from is allowed, this can be restricted to specific ips like 'http:/website.com
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    //The options request which is an HTTP method is always sent first and once by the browser
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+  });
+
+  app.get("/", (req, res, next) => {
+    res.send("Testing root endpoint");
+  });
+
+
+  //HANDLING ERRORS
+  //if the program reaches this line that means no router in products or orders was able to handle the request therefore we catch an err
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    //forward the error
+    next(error);
+  });
+
+  //this line handles error thrown by the previous block or any request that reaches this line.
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error.message
+      }
+    });
+  });
+
+
+
+
+
+
+
+
+console.log(`Listening on port: ${port}`);
+//Start listening for requests on the specified PORT
+server.listen(port);
+
---
+++ back-end/transpiled-server/app.js
@@ -0,0 +1,52 @@
+"use strict";
+
+var _http = _interopRequireDefault(require("http"));
+
+var _express = _interopRequireDefault(require("express"));
+
+var _morgan = _interopRequireDefault(require("morgan"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+//Server Setup
+var port = 3000 || process.env.PORT;
+var app = (0, _express["default"])();
+
+var server = _http["default"].createServer(app); //Express middlewear
+//Funnel all requests through morgan to be logged to the console.
+
+
+app.use((0, _morgan["default"])("dev")); //Parse the data from the requests we get so we can extract them.
+
+app.use(_express["default"].urlencoded({
+  extended: false
+}));
+app.use(_express["default"].json()); //To prevent CORS errors
+//We need to append headers before the response is sent back to the client
+//These headers tell the browser that we allow a client has a different origin from our server to get the response.
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); //* Every origin that a request comes from is allowed, this can be restricted to specific ips like 'http:/website.com
+
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization"); //The options request which is an HTTP method is always sent first and once by the browser
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+
+  next();
+});
+app.get("/", function (req, res, next) {
+  res.send("Testing root endpoint");
+}); //if the program reaches this line that means no router in products or orders was able to handle the request therefore we catch an err
+
+app.use(function (req, res, next) {
+  var error = new Error("Not found");
+  error.status = 404; //forward the error
+
+  next(error);
+});
+console.log("Listening on port: ".concat(port)); //Start listening for requests on the specified PORT
+
+server.listen(port);
