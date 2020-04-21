7dac6a13ed4534db734f284c5f944e27f655f66c
waleedbaroudi <wbaroudi18@ku.edu.tr>
2020-04-21 23:09:11 +0300

    lock main activity orientation

--- app/src/main/java/com/example/eino/controllers/MainActivity.java
+++ app/src/main/java/com/example/eino/controllers/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.transition.ChangeBounds;
 import android.transition.TransitionManager;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.animation.OvershootInterpolator;
 import android.widget.FrameLayout;
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNav;
     FrameLayout fragCont;
+    private boolean onMyProfile = false;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,21 +32,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         bottomNav = findViewById(R.id.bottom_bar);
         fragCont = findViewById(R.id.frag_container);
-        getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, new CategoryFragment()).commit();
+        if (onMyProfile) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, new MyProfileFragment()).commit();
+        } else {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, new CategoryFragment()).commit();
+        }
         bottomNav.setOnNavigationItemSelectedListener(listener);
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener  listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @RequiresApi(api = Build.VERSION_CODES.KITKAT)
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             Fragment selectedFragment = null;
-            switch (item.getItemId()){
-                case R.id.categories_item :
+            switch (item.getItemId()) {
+                case R.id.categories_item:
                     selectedFragment = new CategoryFragment();
+                    onMyProfile = false;
                     break;
-                case R.id.profile_item :
+                case R.id.profile_item:
                     selectedFragment = new MyProfileFragment();
+                    onMyProfile = true;
                     break;
             }
             TransitionManager.beginDelayedTransition(fragCont);
