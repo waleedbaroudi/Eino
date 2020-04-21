3cfa933dafc893e04dc78e78b9c6185621c32b9e
waleedbaroudi <wbaroudi18@ku.edu.tr>
2020-04-22 01:38:15 +0300

    add anim files, add custom fragment animations to main activity

--- app/src/main/java/com/example/eino/controllers/MainActivity.java
+++ app/src/main/java/com/example/eino/controllers/MainActivity.java
@@ -4,14 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.transition.ChangeBounds;
-import android.transition.TransitionManager;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.animation.OvershootInterpolator;
 import android.widget.FrameLayout;
 
 import com.example.eino.R;
@@ -23,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNav;
     FrameLayout fragCont;
-    private boolean onMyProfile = false;
     private static final String TAG = "MainActivity";
+    private boolean onMyProfile = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,42 +29,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         bottomNav = findViewById(R.id.bottom_bar);
         fragCont = findViewById(R.id.frag_container);
-        if (onMyProfile) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, new MyProfileFragment()).commit();
-        } else {
-            getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, new CategoryFragment()).commit();
-        }
         bottomNav.setOnNavigationItemSelectedListener(listener);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart() called");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause() called");
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume() called");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop() called");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy() called");
+        getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, new CategoryFragment()).commit();
     }
 
 
@@ -86,9 +49,12 @@ public class MainActivity extends AppCompatActivity {
                     onMyProfile = true;
                     break;
             }
-            TransitionManager.beginDelayedTransition(fragCont);
-            getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, selectedFragment).commit();
-
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            if (onMyProfile)
+                transaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left).replace(R.id.frag_container, selectedFragment);
+            else
+                transaction.setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right).replace(R.id.frag_container, selectedFragment);
+            transaction.commit();
             return true;
         }
     };
