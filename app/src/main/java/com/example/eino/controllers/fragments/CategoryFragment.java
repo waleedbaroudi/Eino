9e1f0e190b2f1b9603ca56af73a5df8114205edf
waleedbaroudi <wbaroudi18@ku.edu.tr>
2020-04-21 22:41:57 +0300

    fix category adapter after navigation bar merge

--- app/src/main/java/com/example/eino/controllers/fragments/CategoryFragment.java
+++ app/src/main/java/com/example/eino/controllers/fragments/CategoryFragment.java
@@ -11,22 +11,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import com.example.eino.models.MainCategoryAdapter;
+import com.example.eino.models.Category;
+import com.example.eino.models.adapters.MainCategoryAdapter;
 import com.example.eino.R;
+import com.example.eino.models.adapters.MainCategoryAdapter;
+import com.example.eino.models.data_sources.CategoryDataSource;
 
-public class CategoryFragment extends Fragment {
+import java.util.ArrayList;
+
+public class CategoryFragment extends Fragment implements CategoryDataSource.CategoryDataSourceDelegate {
     RecyclerView categoryRecycler;
-
+    CategoryDataSource dataSource;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_category, container, false);
         categoryRecycler = (RecyclerView) view.findViewById(R.id.category_recyclerview);
-        MainCategoryAdapter adapter = new MainCategoryAdapter();
+        dataSource = new CategoryDataSource();
+        dataSource.setDelegate(this);
+        dataSource.fetchCategories();
+        return view;
+    }
+
+    @Override
+    public void categoriesFetched(ArrayList<Category> categories) {
+        MainCategoryAdapter adapter = new MainCategoryAdapter(categories);
         categoryRecycler.setAdapter(adapter);
         categoryRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
-        return view;
+    }
+
+    @Override
+    public void fetchFailure() {
+        Toast.makeText(getContext(), "Failed to load categories", Toast.LENGTH_LONG).show();
     }
 }
