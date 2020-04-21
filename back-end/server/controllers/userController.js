103a8e39b4e2228d88c08c58c9dabb4a7ee7a7e4
poufy <nooraldeenalfayad@hotmail.com>
2020-04-21 20:15:02 +0300

    get users now returns an array of users instead of an object

--- back-end/server/controllers/userController.js
+++ back-end/server/controllers/userController.js
@@ -9,11 +9,11 @@ exports.getUsers = (req, res) => {
     .select()
     .exec()
     .then((users) => {
-      const response = {
-        count: users.length,
-        result: users,
-      };
-      res.status(200).json(response);
+    //   const response = {
+    //     count: users.length,
+    //     result: users,
+    //   };
+      res.status(200).json(users);
     })
     .catch((err) => {
       res.status(500).json({
