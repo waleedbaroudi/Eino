3e39352f2a2f4f9e0927ce2e6cb88216f8577a25
waleedbaroudi <wbaroudi18@ku.edu.tr>
2020-04-21 18:32:56 +0300

    add animation

--- app/src/main/java/com/example/eino/controllers/MainActivity.java
+++ app/src/main/java/com/example/eino/controllers/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.eino.controllers;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.TransitionManager;
 import android.view.MenuItem;
+import android.view.animation.OvershootInterpolator;
+import android.widget.FrameLayout;
 
 import com.example.eino.R;
 import com.example.eino.controllers.fragments.CategoryFragment;
@@ -15,17 +21,20 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNav;
+    FrameLayout fragCont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNav = findViewById(R.id.bottom_bar);
+        fragCont = findViewById(R.id.frag_container);
         getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, new CategoryFragment()).commit();
         bottomNav.setOnNavigationItemSelectedListener(listener);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener  listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             Fragment selectedFragment = null;
@@ -37,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
                     selectedFragment = new MyProfileFragment();
                     break;
             }
-
+            TransitionManager.beginDelayedTransition(fragCont);
             getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, selectedFragment).commit();
 
             return true;
